d85ea5d17238259d5fe9b1305be5ecabed9be519
mases777 <mases@abv.bg>
2022-07-31 11:34:40 +0300

    M07

---
+++ 2021-2022-M07/2022-M07-MVC/ConsoleMVC/Views/Display.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleMVC.Views
+{
+    public class Display
+    {
+        public double Percent { get; set; }
+        public double Amount { get; set; }
+        public double Total { get; set; }
+        public double TipAmount { get; set; }
+        public Display()
+        {
+            Percent = 0;
+            Amount = 0;
+            Total = 0;
+            TipAmount = 0;
+            GetValues();
+        }
+
+        private void GetValues()
+        {
+            Console.Write("Enter the amount of the meal: ");
+            Amount = double.Parse(Console.ReadLine());
+            Console.Write("Enter the percent you want to tip: ");
+            Percent = double.Parse(Console.ReadLine());
+        }
+        public void ShowTipAndTotal()
+        {
+            Console.WriteLine($"Your tip is: {TipAmount:C}");
+            Console.WriteLine($"The total will be {Total:C}");
+        }
+    }
+}
