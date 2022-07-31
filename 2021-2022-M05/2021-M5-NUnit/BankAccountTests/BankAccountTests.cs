6b0b55353431be5041184a4e435deb31e725d803
mases777 <mases@abv.bg>
2022-07-31 10:54:31 +0300

    M05

---
+++ 2021-2022-M05/2021-M5-NUnit/BankAccountTests/BankAccountTests.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zadacha01;
+
+namespace BankAccountTests
+{
+    [TestFixture]
+    public class BankAccountTests
+    {
+        [Test]
+        public void AccountInitializeWithPositiveValue()
+        {
+            BankAccount acount = new BankAccount(2000m);
+            Assert.AreEqual(2000m, acount.Balance);
+        }
+
+        [Test]
+        public void DepositShouldAddMoney()
+        {
+            //Arrange
+            BankAccount account = new BankAccount();
+
+            //Act
+            account.Deposit(50);
+
+            //Assert
+            Assert.IsTrue(account.Balance == 50);
+        }
+
+    }
+}
