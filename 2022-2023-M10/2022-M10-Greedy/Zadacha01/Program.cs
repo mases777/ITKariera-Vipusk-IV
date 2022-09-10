e6329ca1d849cc333eb40d7a7470697292c1ed40
mases777 <mases@abv.bg>
2022-09-10 10:35:30 +0300

    M10

---
+++ 2022-2023-M10/2022-M10-Greedy/Zadacha01/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadacha01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int finalSum = int.Parse(Console.ReadLine());
+            int currentSum = 0;
+            int[] coins = { 50, 50, 50, 20, 20, 20, 10, 10, 10, 5, 5, 5, 2, 2, 2, 1, 1, 1 };
+            Queue<int> resultCoins = new Queue<int>();
+
+            for (int i = 0; i < coins.Length; i++)
+            {
+                if (currentSum + coins[i] > finalSum)
+                {
+                    continue;
+                }
+                currentSum += coins[i];
+                resultCoins.Enqueue(coins[i]);
+                if (currentSum == finalSum)
+                {
+                    Console.Write("Coins: ");
+                    Console.WriteLine(String.Join(", ", resultCoins));
+                    Console.WriteLine($"Count = {resultCoins.Count}");
+                    return;
+                }
+            }
+            Console.WriteLine("Sum not found");
+        }
+    }
+}
