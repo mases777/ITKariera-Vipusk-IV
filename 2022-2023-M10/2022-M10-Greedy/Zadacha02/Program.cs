c459b57673ba572ca20531c27ca50d25911abc37
mases777 <mases@abv.bg>
2022-09-10 10:48:54 +0300

    M10-Zadacha02

---
+++ 2022-2023-M10/2022-M10-Greedy/Zadacha02/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadacha02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int p = int.Parse(Console.ReadLine());
+            int q = int.Parse(Console.ReadLine());
+            int r = 0;
+
+            while (p > 1)
+            {
+                r = (p + q) / p;
+                Console.Write($"1/{r} + ");
+                p = p * r - q;
+                q = q * r;
+                int m = Gcd(p, q);
+                p /= m;
+                q /= m;
+            }
+            Console.WriteLine($"1/{q}");
+        }
+
+        private static int Gcd(int p, int q)
+        {
+            if (p > q)
+            {
+                return Gcd(p - q, q);
+            }
+            else if (q > p)
+            {
+                return Gcd(p, q - p);
+            }
+            return p;
+        }
+    }
+}
