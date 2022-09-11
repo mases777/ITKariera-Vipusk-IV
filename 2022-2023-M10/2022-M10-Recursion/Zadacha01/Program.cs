23148eaf3a484b12f7372cbec9234328a2a8d8d5
mases777 <mases@abv.bg>
2022-09-11 10:35:16 +0300

    M10-Recursion

---
+++ 2022-2023-M10/2022-M10-Recursion/Zadacha01/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadacha01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Въведете цяло число по-голямо от 2: ");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine(Fib(n));
+        }
+
+        private static int Fib(int n)
+        {
+            if (n == 1 || n == 2)
+            {
+                return 1;
+            }
+            else
+            {
+                return Fib(n - 1) + Fib(n - 2);
+            }
+        }
+    }
+}
---
+++ 2022-2023-M10/2022-M10-Recursion/Zadacha02/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadacha02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Въведете цяло число А: ");
+            int a = int.Parse(Console.ReadLine());            
+            Console.Write("Въведете цяло число B: ");
+            int b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Най-големия общ делител на А и В е = {Gcd(a, b)}");
+        }
+
+        private static int Gcd(int a, int b)
+        {
+            if (a == b)
+            {
+                return a;
+            }
+            if (a > b)
+            {
+                return Gcd(a - b, b);
+            }
+            return Gcd(a, b - a);
+        }
+    }
+}
