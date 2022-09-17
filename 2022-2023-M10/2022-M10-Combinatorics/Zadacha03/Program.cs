718980b2eda0af2f5360673f04100f192b00c643
mases777 <mases@abv.bg>
2022-09-17 10:53:44 +0300

    M10-Zadacha03

---
+++ 2022-2023-M10/2022-M10-Combinatorics/Zadacha03/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadacha03
+{
+    class Program
+    {
+        static List<int> currentSolution = new List<int>();
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Solve(n, 0);
+        }
+
+        private static void Solve(int n, int index)
+        {
+            if (n <= 0)
+            {
+                for (int i = 0; i < currentSolution.Count-1; i++)
+                {
+                    if (currentSolution[i] < currentSolution[i+1])
+                    {
+                        return;
+                    }
+                }
+                Console.WriteLine(String.Join(" + ", currentSolution));
+                return;
+            }
+            for (int i = n; i > 0; i--)
+            {
+                currentSolution.Add(i);
+                Solve(n - i, index + 1);
+                currentSolution.RemoveAt(index);
+            }
+        }
+    }
+}
