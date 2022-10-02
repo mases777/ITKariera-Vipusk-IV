202660b14d1735539a72c9bb3257bb4e29dfa4dc
mases777 <mases@abv.bg>
2022-10-02 10:37:28 +0300

    M10-Dynamic

---
+++ 2022-2023-M10/2022-M10-Dynamic-programming/Zadacha01/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadacha01
+{
+    internal class Program
+    {
+        static BigInteger counter = 0;
+        static int[] sizes;
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var length = input[0];
+            sizes = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Solve(length);
+            Console.WriteLine(counter);
+        }
+
+        private static void Solve(int length)
+        {
+            if (length == 0)
+            {
+                counter++;
+                return;
+            }
+            else if (length > 0)
+            {
+                for (int i = 0; i < sizes.Length; i++)
+                {
+                    if (length - sizes[i] >= 0 )
+                    {
+                        Solve(length - sizes[i]);
+                    }
+                }
+            }
+        }
+    }
+}
